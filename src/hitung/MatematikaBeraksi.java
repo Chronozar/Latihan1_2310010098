76d25fe5767e8a638a3ecae89f13cae4245a81e2
Chronozar <memitriadi@gmail.com>
2025-06-11 00:46:34 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author Chronox
- */
 public class MatematikaBeraksi {
     
+    public static void main(String[] args) {
+        // membuat objek
+        Matematika ifah = new Matematika(5, 3);
+
+        System.out.println("Hasil Penjumlahan : " + ifah.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + ifah.setPengurangan());
+        System.out.println("Hasil Perkalian   : " + ifah.setPerkalian());
+        System.out.println("Hasil Pembagian   : " + ifah.setPembagian());
+    }
 }
+    
+
