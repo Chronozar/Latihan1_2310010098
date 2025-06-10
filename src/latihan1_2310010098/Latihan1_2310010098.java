b956d49bed208ccd25c96cf22a01b62821079dca
Chronozar <memitriadi@gmail.com>
2025-06-10 23:23:52 +0800

    initial commit

---
+++ src/latihan1_2310010098/Latihan1_2310010098.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan1_2310010098;
+
+/**
+ *
+ * @author Chronox
+ */
+public class Latihan1_2310010098 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
